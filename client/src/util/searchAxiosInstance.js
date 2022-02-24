b12afef8b90993964918c428f9db0ede0bb0c9ac
Riccardo Limiti <riccardo.limiti@gmail.com>
2022-02-24 12:25:06 +0100

    change

--- client/src/util/searchAxiosInstance.js
+++ client/src/util/searchAxiosInstance.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 const searchAxiosInstance = axios.create();
 
 searchAxiosInstance.defaults.baseURL = 'http://openlibrary.org/search.json?';
-searchAxiosInstance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+// searchAxiosInstance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+// searchAxiosInstance.defaults.headers.post
+// ['Access-Control-Allow-Origin'] = '*'
+// ['Content-Type'] = 'application/json'
+
+// searchAxiosInstance.defaults.withCredentials = true;
 
 export default searchAxiosInstance;
